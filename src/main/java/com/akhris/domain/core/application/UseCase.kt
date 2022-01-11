708bc920faa69c28c9cdbcf6ecb1992e02a2abcd
anatoly <khristianovsky@gmail.com>
2022-01-11 07:32:46 +0300

    initial commit

---
+++ src/main/java/com/akhris/domain/core/application/UseCase.kt
@@ -0,0 +1,31 @@
+package com.akhris.domain.core.application
+
+import kotlinx.coroutines.CoroutineDispatcher
+import kotlinx.coroutines.withContext
+
+
+abstract class UseCase<out R, in P>(private val coroutineDispatcher: CoroutineDispatcher) where R : Any {
+    suspend operator fun invoke(params: P): Result<R> {
+        return try {
+            // Moving all use case's executions to the injected dispatcher
+            // In production code, this is usually the Default dispatcher (background thread)
+            // In tests, this becomes a TestCoroutineDispatcher
+            withContext(coroutineDispatcher) {
+                run(params).let {
+                    Result.Success(it)
+                }
+            }
+        } catch (e: Exception) {
+            Result.Failure(e)
+        }
+    }
+
+    /**
+     * Override this to set the code to be executed.
+     */
+    @Throws(RuntimeException::class)
+    abstract suspend fun run(params: P): R
+
+
+    class None
+}
