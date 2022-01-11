843bd237927368777836e318cb5d9706f348af27
anatoly <khristianovsky@gmail.com>
2022-01-11 08:38:09 +0300

    added some utils

--- src/main/java/com/akhris/domain/core/utils/ResultUtils.kt
+++ src/main/java/com/akhris/domain/core/utils/ResultUtils.kt
@@ -1,7 +1,6 @@
 package com.akhris.domain.core.utils
 
 import com.akhris.domain.core.application.Result
-import com.akhris.domain.core.exceptions.NotFoundInRepositoryException
 
 /**
  * If result is [Result.Success] - returns result value, if not - return null.
@@ -19,3 +18,8 @@ fun <T> Result<T>.unpack(whatNotFoundInRepo: String): T = when (this) {
     is Result.Success -> value
     is Result.Failure -> throw throwable
 }
+
+/**
+ * Unpacks list of results to list of values or empty list if there is no [Result.Success]
+ */
+fun <T> List<Result<T>>.unpack(): List<T> = mapNotNull { it.unpack() }
