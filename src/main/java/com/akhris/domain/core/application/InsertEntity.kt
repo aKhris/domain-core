aff963592898643c88b8b3744283f284a74b0421
Anatoly Khristianovsky <khristianovsky@gmail.com>
2022-03-25 19:33:09 +0300

    Added Copy Param to InsertEntity.kt

--- src/main/java/com/akhris/domain/core/application/InsertEntity.kt
+++ src/main/java/com/akhris/domain/core/application/InsertEntity.kt
@@ -11,6 +11,7 @@ import kotlinx.coroutines.CoroutineDispatcher
  */
 open class InsertEntity<ID, ENTITY : IEntity<ID>>(
     private val repo: IRepository<ID, ENTITY>,
+    private val entityCopier: ((ENTITY) -> ENTITY)? = null,
     @IoDispatcher
     ioDispatcher: CoroutineDispatcher
 ) : UseCase<ENTITY, InsertEntity.Params>(ioDispatcher) {
@@ -19,6 +20,8 @@ open class InsertEntity<ID, ENTITY : IEntity<ID>>(
         return when (params) {
             is Insert -> (params.entityToInsert as? ENTITY)?.let { insert(params.entityToInsert) }
                 ?: throw IllegalArgumentException("Entity to insert is not type of insert use case: $this")
+            is Copy -> (params.entityToCopy as? ENTITY)?.let { copy(params.entityToCopy) }
+                ?: throw IllegalArgumentException("Entity to copy is not type of insert use case: $this")
         }
     }
 
@@ -27,7 +30,16 @@ open class InsertEntity<ID, ENTITY : IEntity<ID>>(
         return entity
     }
 
+    private suspend fun copy(entity: ENTITY): ENTITY {
+        val copiedEntity = entityCopier?.invoke(entity)
+            ?: throw IllegalArgumentException("To copy an entity entityCopier must be provided: $this")
+        repo.insert(copiedEntity)
+        return copiedEntity
+    }
+
+
     sealed class Params
     data class Insert(val entityToInsert: Any) : Params()
+    data class Copy(val entityToCopy: Any) : Params()
 
 }
